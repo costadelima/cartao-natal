918bcd43885b8373088a875457693188a6d767a9
Luiz Flavio <T0116158@vli-logistica.com.br>
2024-12-20 17:21:41 -0300

    Cartao natalino Scar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import caartaonatalscar from './caartaonatalscar.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Feliz Natal</h1>
+      <div className="lights">
+        <img src={caartaonatalscar} alt="Imagem de fundo" className="background-image" />
+        <div className="lightCima"></div>
+        <div className="lightBaixo"></div>
+        <div className="lightCima"></div>
+        <div className="lightBaixo"></div>
+        <div className="lightCima"></div>
+        <div className="lightBaixo"></div>
+        <div className="lightCima"></div>
+        <div className="lightBaixo"></div>
+        <div className="lightCima"></div>
+        <div className="lightBaixo"></div>
+      </div>
     </div>
   );
 }
